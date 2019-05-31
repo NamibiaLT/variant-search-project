d0601adea5bed40a520812be3c7b178ba534cde7
NLT <NamibiaLT@users.noreply.github.com>
2019-05-31 11:04:17 -0700

    add templates to views file

--- variant_search_app/backend/views.py
+++ variant_search_app/backend/views.py
@@ -5,5 +5,17 @@ import pandas as pd
 
 backend_blueprint = Blueprint("backend", __name__)
 # unzipped_gene_file = ZipFile.extractall(path="/data/variants.tsv.zip", members=None, pwd=None)
-gene_file = pd.read_csv("/data/variants.tsv", sep = " ")
+gene_file = pd.read_csv("variants.tsv", sep = "/t")
 
+# INDEX TEMPLATE -  where search bar will be
+@backend_blueprint.route("/")
+def index():
+    return render_template('index.html')
+
+# SHOW ACTION TEMPLATE- where we send users after they hit submit
+@backend_blueprint.route("/company_results")
+def show():
+    company_id = request.args.get("SAMPLE", type=str) # Pass in string as the argument name in get
+    data = get_company_data(company_id)
+    # ['google', 'amazon', 'facebook', 'netflix', 'apple', 'microsoft']
+    return render_template('company_results.html', context=data)
